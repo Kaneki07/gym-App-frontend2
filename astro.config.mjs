77b82fe34cfb67d11b3901df3eeae160210070a1
Kaneki07 <direlrodriguez@gmail.com>
2026-01-26 00:59:23 -0400

    fix y agregado vercel en

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel/serverless'; //
 
 export default defineConfig({
   integrations: [react()],
+  output: 'server', // Esto soluciona el error de rutas dinámicas
+  adapter: vercel(), // Esto le dice a Astro que el destino es Vercel
   vite: {
     plugins: [tailwindcss()],
   },
